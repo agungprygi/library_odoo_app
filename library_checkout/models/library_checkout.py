908de7d6b8852defaad2e3258b4c497cf283a113
Yogi <agungprayogi305@gmail.com>
2024-08-07 17:27:16 +0700

    add model and view

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -27,6 +27,8 @@ class Checkout(models.Model):
         return stages.search([], order=order)
 
     member_id = fields.Many2one("library.member", required=True)
+    name = fields.Char(string="Title")
+    member_image = fields.Binary(related='member_id.image_128')
     user_id = fields.Many2one("res.users", "Librarian", default=lambda s: s.env.user)
     line_ids = fields.One2many("library.checkout.line", "checkout_id", string="Borrowed Books")
     request_date = fields.Date(
@@ -45,6 +47,24 @@ class Checkout(models.Model):
     checkout_date = fields.Date(readonly=True)
     close_date = fields.Date(readonly=True)
 
+    count_checkouts = fields.Integer(compute="_compute_count_checkouts")
+
+    def _compute_count_checkouts(self):
+        members = self.mapped("member_id")
+        domain = [("member_id", "=", members.ids),
+                  ("state", "not in", ["done", "cancel"])]
+        raw = self.read_group(domain, ["id:count"], ["member_id"])
+        data = {x["member_id"][0]: x["member_id_count"] for x in raw}
+        for checkout in self:
+            checkout.count_checkouts = data.get(checkout.member_id.id, 0)
+
+    num_books = fields.Integer(compute="_compute_num_books", store=True)
+
+    @api.depends('line_ids')
+    def _compute_num_books(self):
+        for book in self:
+            book.num_books = len(book.line_ids)
+
     @api.model
     def create(self, vals):
         new_record = super().create(vals)
@@ -61,4 +81,11 @@ class Checkout(models.Model):
                 self.with_context(_checkout_write=True).write({"checkout_date":fields.Date.today()})
             if new_state != old_state and new_state == "done":
                 self.with_context(_checkout_write=True).write({"close_date": fields.Date.today()})
+        return True
+
+    def button_done(self):
+        Stage = self.env["library.checkout.stage"]
+        done_stage = Stage.search([("state","=","done")], limit=1)
+        for checkout in self:
+            checkout.stage_id = done_stage
         return True
--- library_checkout/wizard/checkout_mass_message.py
+++ library_checkout/wizard/checkout_mass_message.py
@@ -1,4 +1,7 @@
+import logging
 from odoo import fields, models, api, exceptions
+
+_logger = logging.getLogger(__name__)
 
 class CheckoutMassMessage(models.TransientModel):
     _name = "library.checkout.massmessage"
@@ -7,3 +10,38 @@ class CheckoutMassMessage(models.TransientModel):
     checkout_ids = fields.Many2many("library.checkout", string="Checkout")
     message_subject = fields.Char()
     message_body = fields.Html()
+
+    @api.model
+    def default_get(self, fields):
+        default_dict = super(CheckoutMassMessage, self).default_get(fields)
+        # add values to the default_dict here
+        checkout_ids = self.env.context["active_ids"]
+        default_dict["checkout_ids"] = [(6, 0, checkout_ids)]
+        return default_dict
+
+    def button_send(self):
+        self.ensure_one()
+        
+        if not self.checkout_ids:
+          raise exceptions.UserError("No Checkouts were selected.")
+        
+        if not self.message_body:
+          raise exceptions.UserError("A message body is required")
+
+        for checkout in self.checkout_ids:
+            checkout.message_post(
+                body=self.message_body,
+                subject=self.message_subject,
+                subtype='mail.mt_comment'
+            )
+            _logger.debug(
+              "Message on %d to follower: %s",
+              checkout.id,
+              checkout.message_follower_ids
+            )
+        _logger.info(
+          "Posted %d messages to the Checkouts: %s",
+          len(self.checkout_ids),
+          str(self.checkout_ids)
+        )
+        return True
